254f9254732b10b72be400a987b0c6b1a702e8c0
JosefNagelschmidt <login@jn.contact>
2022-12-29 19:15:52 +0100

    Update board and check for win

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from setup import read_player_input
+import sys
+
+from setup import check_for_win, print_board, read_player_input, update_board
 
 if __name__ == "__main__":
-    pass
+    win = False
+    state = ["#"] * 9
+
+    while not win:
+        print_board(state=state)
+        user_input = read_player_input()
+        if check_for_win(state=state, user_input=user_input):
+            state = update_board(state=state, user_input=user_input)
+            print_board(state=state)
+            print("Winner!")
+            sys.exit()
+        state = update_board(state=state, user_input=user_input)
--- setup.py
+++ setup.py
@@ -5,9 +5,52 @@ def print_board(state: list[str]):
         else:
             print(sign, end='')
 
-def read_player_input():
+def read_player_input() -> tuple[str, int]:
     eingabe = "A"
     while eingabe not in ["X", "O"]:
         eingabe = input("Bitte wähle zwischen X oder O aus:\n")
-    return eingabe
+
+    position = -1
+    while position not in range(1,10):
+        position = input("Bitte wähle eine Position:\n")
+        try:
+            position = int(position)
+        except:
+            print("Cannot convert to integer. Please enter valid input.")
+    
+    return (eingabe, position)
+
+
+def update_board(state: list[str], user_input: tuple[str, int]):
+    position = user_input[1] - 1
+    state[position] = user_input[0]
+    return state
+
+
+def check_for_win(state: list[str], user_input: tuple[str, int]) -> bool:
+    state = update_board(state=state, user_input=user_input)
+    # check if first row same
+    if len(set(state[0:3])) == 1 and set(state[0:3]) != set("#"): 
+        return True
+    # check second row
+    if len(set(state[3:6])) == 1 and set(state[3:6]) != set("#"): 
+        return True
+    # check third row
+    if len(set(state[6:9])) == 1 and set(state[6:9]) != set("#"): 
+        return True
+    # check first column
+    if len(set(state[0::3])) == 1 and set(state[0::3]) != set("#"): 
+        return True
+    # check second column
+    if len(set(state[1::3])) == 1 and set(state[1::3]) != set("#"): 
+        return True
+    # check third column
+    if len(set(state[2::3])) == 1 and set(state[2::3]) != set("#"): 
+        return True
+    if len(set(state[0::4])) == 1 and set(state[0::4]) != set("#"): 
+        return True
+    if len(set(state[2::2])) == 1 and set(state[2::2]) != set("#"): 
+        return True
+
+    return False
 
